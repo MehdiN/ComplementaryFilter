e43a269754c380bd92ae25e299f33f19da25a38c
mehdi <nima.mehdi@ensta-bretagne.org>
2019-07-28 20:07:06 +0200

    added features

--- include/ComplementaryFilter.hpp
+++ include/ComplementaryFilter.hpp
@@ -1,3 +1,6 @@
+#ifndef _COMPLEMENTARY_FILTER_HPP_
+#define _COMPLEMENTARY_FILTER_HPP_
+
 #include <cmath>
 #include <stdio.h>
 #include <vector>
@@ -7,36 +10,33 @@
 #include <boost/math/special_functions.hpp>
 
 
-#define LOCAL_MAG_X 225.1 //miligauss
-#define LOCAL_MAG_Y 5.75
-#define LOCAL_MAG_Z 415.2
+// #define LOCAL_MAG_X 225.1 //miligauss
+// #define LOCAL_MAG_Y 5.75
+// #define LOCAL_MAG_Z 415.2
 
-constexpr double gravity = 9.81;
+constexpr double gravity = 9.80665;
+constexpr double local_mx = 208.762; //in miligauss
+constexpr double local_my = 3.448;
+constexpr double local_mz = 434.129;
 
 using namespace std;
 
 
-
-class ComplementaryFilter{
+    class ComplementaryFilter{
 
         // constructor
-            ComplementaryFilter()
+        public:
+
+        ComplementaryFilter()
             {
+                is_init = false;
+                data_fused = false;
+                filter_updated = false;
+                magFieldEarth = Eigen::Vector3d(local_mx,local_my,local_mz);
                 mag = Eigen::Vector3d(0,0,0);
-                acc = Eigen::Vector3d(0,0,0);
+                accel = Eigen::Vector3d(0,0,0);
                 gyro = Eigen::Vector3d(0,0,0);
             }
-
-        typedef Eigen::Matrix<double,4,1> Pose;
-
-        struct flags
-        {
-            bool mag_data_available;
-            bool mag_data_updated;
-            bool gyro_data_updated;
-            bool acc_data_updated;
-            bool filter_update;
-        };
 
         struct parameters
         {
@@ -48,36 +48,25 @@ class ComplementaryFilter{
             double sat;
         };
         
-
-
-        public:
-
         // Maths Functions
-        Eigen::Matrix3d quatToRotationMatrix(Pose quat);  // transforme quaternion to rotation matrix
-        // Pose normalizeQuaternion(Pose quat);  // normalize quaternion
-        // Pose crossfunction(Pose q1,Pose q2);  // quaternion cross function
-        //Eigen::Vector3d saturator(Eigen::Vector3d x,double d){return std::min(1.0,d/x.norm()*x);}
+        Eigen::Matrix3d quatToRotationMatrix(Eigen::Vector4d quat);  // transforme quaternion to rotation matrix
 
         // Filter Methods
+        void initFilter();
         void updateSensorData(Eigen::Vector3d gyro, Eigen::Vector3d accel, Eigen::Vector3d mag);
         void fuseInertialData();
         void updateFilter();
 
-
-
-    
-
         private:
 
-        float time_step;
+        // filter variables;
+        double dt; 
         Eigen::Vector3d magFieldEarth;
         Eigen::Matrix3d rotationMatrix;
         Eigen::Vector3d mag;
         Eigen::Vector3d accel;
         Eigen::Vector3d gyro;
         Eigen::Vector3d bias;
-        Pose quat;
-        Pose update_quat;
         Eigen::Vector3d sigR;
         Eigen::Vector3d sigB;
         Eigen::Vector3d gyro_estimate;
@@ -85,10 +74,14 @@ class ComplementaryFilter{
         Eigen::Matrix3d rotation_matrix;
 
         // filter flags
-        flags filterFlags;
+        bool is_init;
+        bool data_fused;
+        bool filter_updated;
 
-        // filter coefficients
+        // filter parameters
         parameters params;
-        double dt;
 
-}
+    };
+
+
+#endif
--- src/ComplementaryFilter.cpp
+++ src/ComplementaryFilter.cpp
@@ -2,9 +2,18 @@
 
 /*
 ||===========================================||
-||===============Main Functions==============||
+||===============Filter Methods==============||
 ||===========================================||
 */
+
+
+void ComplementaryFilter::initFilter()
+{
+
+
+    is_init = true;
+}
+
 
 void ComplementaryFilter::updateSensorData(Eigen::Vector3d gyro, Eigen::Vector3d accel, Eigen::Vector3d mag)
 {
@@ -18,28 +27,29 @@ void ComplementaryFilter::updateSensorData(Eigen::Vector3d gyro, Eigen::Vector3d
 void ComplementaryFilter::fuseInertialData()
 {
 
-    // declare local variable used to fuse data
+    // local variables used for data fusion
     Eigen::Vector3d u_body,u_inertial,v_body,v_inertial,v_estimate,u_estimate; 
     Eigen::Matrix3d projUb,projUi;
 
     u_body = -1.0 * accel / gravity;
     u_inertial = Eigen::Vector3f(0,0,1);
 
-    projUb = pow(u_body.norm(),2)*Eigen::Matrix3d::Identity() - u_body*u_body.transpose();
-    projUi = pow(u_body.norm(),2)*Eigen::Matrix3d::Identity() - u_body*u_body.transpose();
+    // compute the orthogonal projection of vector uBody and uInertial
+    projUb = pow(u_body.norm(),2)*Eigen::Matrix3d::Identity() - u_body * (u_body.transpose());
+    projUi = pow(u_body.norm(),2)*Eigen::Matrix3d::Identity() - u_body * (u_body.transpose());
 
     v_body = (projUb*mag)/(projUi*magFieldEarth).norm();
     v_inertial = (projUi*magFieldEarth)/(projUi*magFieldEarth).norm();
     
-    // check consitancy of the applied method
+    // Compute gravity and magnetic field estimate in body frame
     u_estimate = quaternion.toRotationMatrix().transpose()*u_inertial;
     v_estimate = quaternion.toRotationMatrix().transpose()*v_inertial;
 
     // Fuse the data;
-
     sigR = params.k1 * u_body.cross(u_estimate) + params.k2 * ( u_estimate.dot(u_estimate.transpose()) ).dot(v_body.cross(v_estimate)) ;
     sigB = params.k3 * u_body.cross(u_estimate) - params.k4 * v_body.cross(v_estimate);
-
+    
+    // Correct the gyro reading with the previous bias and the fused data
     gyro_estimate = gyro - bias + sigR;
 
 }
@@ -58,8 +68,8 @@ void ComplementaryFilter::updateFilter()
 
     quaternion = (cos( 0.5*dt*gyro_estimate.norm() ) * Eigen::Matrix4d::Identity() + 0.5*dt*boost::math::sinc_pi(0.5*dt*gyro_estimate.norm())*A_rate)*quaternion;
 
-    Eigen::Vector3d bias_sat = std::min(1.0,params.sat/bias.norm());
-    bias += dt * (-params.kb*bias + param.kb*bias_sat + sigB );
+    Eigen::Vector3d bias_sat = std::min(1.0,params.sat/bias.norm())*bias;
+    bias += dt * (-params.kb*bias + params.kb*bias_sat + sigB );
 
     quaternion.normalize();
 
@@ -72,7 +82,7 @@ void ComplementaryFilter::updateFilter()
 */
 
 
-Eigen::Matrix3d ComplementaryFilter::quatToRotationMatrix(Pose q)
+Eigen::Matrix3d ComplementaryFilter::quatToRotationMatrix(Eigen::Vector4d q)
 {
 
     Eigen::Matrix3d R;
